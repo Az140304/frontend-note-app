624e1a655c14c1d64d10b78d100a606cc23a4fd2
afharyo <afharyodiwseno@gmail.com>
2025-03-22 13:24:01 +0700

    upload frontend note app

---
+++ index.js
@@ -0,0 +1,28 @@
+import express, { response } from "express";
+import cors from "cors";
+import NoteRoute from "../backend/routes/NoteRoute.js";
+import bodyParser from "body-parser"
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { dirname } from 'path';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(cors());
+
+app.use(express.json());
+app.use(NoteRoute);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(3000, () => console.log("Frontend server connected"));
+
+app.get('/homenote', (req,res) => {
+    res.render('home');
+})
+

02579dae175feb96e29fb1e054eb976e910f322b
Az140304 <128491860+Az140304@users.noreply.github.com>
2025-04-15 23:52:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express, { response } from "express";
 import cors from "cors";
-import NoteRoute from './backend/routes/NoteRoute.js';
 import bodyParser from "body-parser"
 import path from 'path';
 import {fileURLToPath} from 'url';
@@ -10,12 +9,10 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
-
 app.use(cors());
 
 app.use(express.json());
-app.use(NoteRoute);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -27,4 +24,3 @@ app.listen(3000, () => console.log("Frontend server connected"));
 app.get('/homenote', (req,res) => {
     res.render('home');
 })
-

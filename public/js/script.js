4eefedf0c6dad61c364d1900dd1727ef090feca4
Az140304 <128491860+Az140304@users.noreply.github.com>
2025-04-15 22:41:28 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://noteapp-be-746828199699.us-central1.run.app/notes";
+const apiUrl = "http://localhost:5000/notes";
 
 document.getElementById("userForm").addEventListener("submit", async (e) => {
     e.preventDefault();
